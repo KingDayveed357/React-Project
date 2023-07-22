c0b398cf017dff1eecb337a4a15225d24e721953
jennifer-pecky <ugwojennifer7@gmail.com>
2023-07-22 12:52:41 +0100

    app

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,14 +1,14 @@
-import React from 'react';
+
 
 function Services() {
   return (
     <>
-    <div className='bg-[#F3F8FA]'>
-    <div className='flex justify-between max-w-screen-xl mx-auto py-5 md:px-3 px-7'>
-     <h1 className='text-2xl'>About Us</h1>
-      <p> <a href='/' className='text-[#45BEFF] hover:text-[#3ba1d8] p-1 text-center'>Home</a> / Services</p>
-    </div>
-    </div>
+      <div className='bg-[#F3F8FA]'>
+        <div className='flex justify-between max-w-screen-xl mx-auto py-5 md:px-3 px-7'>
+          <h1 className='text-2xl'>Our Services</h1>
+          <p> <a href='/' className='text-[#45BEFF] hover:text-[#3ba1d8] p-1 text-center'>Home</a> / Services</p>
+        </div>
+      </div>
     </>
   )
 }
